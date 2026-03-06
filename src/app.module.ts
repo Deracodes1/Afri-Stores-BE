abdadfa0506ea1f4ce95baffc423780dbe54e3e4
Deracodes1 <ezemeniachidera@gmail.com>
2026-03-06 05:15:17 -0800

    added authorization middleware as well as dto for delete product route for enhnaced errror handling

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,34 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
+import { AuthMiddleware } from './common/middleware/auth/auth.middleware';
 
 @Module({
   imports: [ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes(
+      {
+        path: 'product/:id',
+        method: RequestMethod.DELETE,
+      },
+      {
+        path: 'product',
+        method: RequestMethod.POST,
+      },
+      {
+        path: 'product/:id',
+        method: RequestMethod.PATCH,
+      },
+    );
+  }
+}
