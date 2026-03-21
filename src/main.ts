c5879ba21a4fd1a9911e2192aafc281589ce723e
Deracodes1 <ezemeniachidera@gmail.com>
2026-03-21 03:26:35 -0700

    enebled global app versioning and cors enabled for localhost 4200

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // making the validators and transformers available globally
@@ -11,6 +11,28 @@ async function bootstrap() {
       transform: true, // Automatically transforms payloads to match DTO types
     }),
   );
+
+  // the specified forntend urls in the origin array will able to communiccate
+  //  with my nest application
+  app.enableCors({
+    origin: ['http://localhost:4200'],
+    credentials: true, //will enable the permitted frontend apps to send access toekn via authorization headers
+  });
+
+  // this will enable global versioning in our nest js application.
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
+
+  // to enable global prefix of /api before the version
+  app.setGlobalPrefix('api');
+  // this will enable graceful shutdown of the app(finish pending tasks before it does)
+  // made it to run only during production cos it consumes a small amount of emmory
+  if (process.env.NODE_ENV === 'production') {
+    app.enableShutdownHooks();
+  }
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
@@ -24,5 +46,5 @@ bootstrap().catch((err) => {
 //  above is a helper command i will run in terminal to generate a random crypto string as my
 // secret key
 
-//CREATE DATABASE project_name;
 //  this is for creating a new db in pgadmin
+//CREATE DATABASE project_name;
